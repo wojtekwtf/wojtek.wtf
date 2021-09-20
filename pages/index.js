5d6c471f808d3c983362d22943949c2ab47a2ed9
wkulikowski <wojciechkulikowski@icloud.com>
2021-09-20 17:15:34 +0200

    Add profile pic

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,15 @@ export default function Home() {
       <main className="flex flex-col items-center w-full flex-1 px-8 md:px-20 text-center font-mono">
         <h1 className="text-6xl md:text-5xl font-semibold mt-20 mb-4 md:mb-2">hi im wojtek</h1>
         <p className="text-sm md:text-xs mb-10">(it's a polish name pronounced voytech 🇵🇱)</p>
-        <p className="mb-10">im a software dev & founder of Mazury, a decentralized p2p referral network on ethereum</p>
-        <p className="mb-10">in my web2 life i used to <a target="_blank" href="https://www.toptal.com/resume/wojciech-kulikowski" className="underline">freelance</a> and founded a <a target="_blank" href="https://www.deltologic.com/" className="underline">software agency</a></p>
+        <p className="mb-10 w-4/5 max-w-screen-sm">im a software dev & founder of Mazury Labs, a collaborative effort for building decentralized professional identities & enabling global web3 talent</p>
+        <p className="mb-10">in my web2 life I was <a target="_blank" href="https://www.toptal.com/resume/wojciech-kulikowski" className="underline">freelancing</a> and cofounded a <a target="_blank" href="https://www.deltologic.com/" className="underline">software agency</a></p>
         <p className="mb-10">i used to live in berlin & warsaw, now im based in paris 🇫🇷</p>
         <a target="_blank" href="https://twitter.com/wojtekwtf" className="underline mb-4">twitter</a>
-        <a target="_blank" href="https://app.ens.domains/name/wojtek.eth" className="underline">ens</a>
+        <a target="_blank" href="https://app.ens.domains/name/wojtek.eth" className="underline mb-10">ens</a>
+        <figure className="mb-20">
+          <img src="/wojtek.png" className="w-4/5 mx-auto md:w-80" />
+          <figcaption className="text-xs mt-2">this is me. photo taken at irl <a target="_blank" href="https://en.wikipedia.org/wiki/Masuria" className="underline">Mazury</a></figcaption>
+        </figure>
       </main>
     </div>
   )
