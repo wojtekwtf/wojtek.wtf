025c99864bc1fed56ff5187de7e6df676a62d528
wkulikowski <wojciechkulikowski@icloud.com>
2021-10-07 14:56:36 +0200

    Add plausible analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import PlausibleProvider from 'next-plausible'
+
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain="wojtek.wtf">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  )
 }
 
 export default MyApp
